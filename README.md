8f9c700e6f7f8fc0a5b5cab6d3184e4d79ebc030
Kirill test <kgdtln@gmail.com>
2023-05-24 23:39:42 +0300

    adding updated script and add description on README file

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# FIPS-140
+# FIPS-140 Randomness Test Suite
+
+This Python code implements the FIPS-140 Randomness Test Suite. It includes four tests to evaluate the randomness of a bit sequence: Monobit Test, Longest Run Test, Poker Test, and Runs Test. The code uses the `secrets` and `re` modules to generate random bit sequences and perform the tests.
+
+## Usage
+
+1. Clone the repository or download the code files.
+2. Run the `fips_140.py` file using Python (version 3.x).
+3. The code will generate a random bit sequence and perform the FIPS-140 tests on it.
+4. The test results will be displayed in the console.
+
+## Test Results
+
+The test results will indicate whether the generated bit sequence passes or fails each test. If all four tests pass, the sequence is considered sufficiently random. Otherwise, specific test failures will be reported.
+
+### Monobit Test
+
+The Monobit Test checks if the number of '1's in the sequence falls within an acceptable range. The acceptable range for a 20,000-bit sequence is 9,654 to 10,346.
+
+### Longest Run Test
+
+The Longest Run Test examines the length of the longest run of '0's and '1's in the sequence. The maximum acceptable length for each run is 34.
+
+### Poker Test
+
+The Poker Test splits the sequence into segments and counts the occurrences of each segment. It computes the X3 statistic based on these counts and compares it to an acceptable range (1.03 to 57.4).
+
+### Runs Test
+
+The Runs Test counts the number of runs of each length from 1 to 6 in the sequence and compares them to acceptable ranges specified by FIPS-140. The acceptable ranges are as follows:
+
+- Runs of length 1: 2,267 to 2,733
+- Runs of length 2: 1,079 to 1,421
+- Runs of length 3: 502 to 748
+- Runs of length 4: 223 to 402
+- Runs of length 5: 90 to 223
+- Runs of length 6 and above: 90 to 223
+
+## To DO:
+
+- fixing Poker Test.
+- confirm what Run Test is working properly
--- fips.py
+++ fips.py
@@ -2,33 +2,78 @@ import secrets
 import re
 
 def generate_random_sequence():
+    # Generate 20000 random bits (2500 bytes)
     return secrets.token_bytes(2500)
 
 def monobit_test(bits):
+    # Count the number of '1's in the sequence
     count = sum(bin(byte).count('1') for byte in bits)
+
+    # Check if the number of '1's falls within the acceptable range
     print(f'Monobit test: count is {count}, acceptable range is 9654 to 10346, test {"passed" if 9654 <= count <= 10346 else "failed"}')
     return 9654 <= count <= 10346
 
 def longest_run_test(bits):
-    max_run_length = max(len(run) for run in ''.join(bin(byte)[2:].zfill(8) for byte in bits).split('0') + ''.join(bin(byte)[2:].zfill(8) for byte in bits).split('1'))
-    return max_run_length <= 34  # Прийнятний діапазон для 20000 бітів
 
+    bit_string = ''.join(f'{byte:08b}' for byte in bits)
+
+    # Find the length of the longest run of '0's and '1's
+    longest_zero_run = max(len(run) for run in re.findall(r'0+', bit_string))
+    longest_one_run = max(len(run) for run in re.findall(r'1+', bit_string))
+
+    print(f'Longest run of zeros: length is {longest_zero_run}, maximum acceptable length is 34, test {"passed" if longest_zero_run <= 34 else "failed"}')
+    print(f'Longest run of ones: length is {longest_one_run}, maximum acceptable length is 34, test {"passed" if longest_one_run <= 34 else "failed"}')
+    
+    return longest_zero_run <= 34 and longest_one_run <= 34
+
+# Here I have problem with implementing a formula from task, so probably it not fully work.
 def poker_test(bits):
-    frequencies = [0]*16
-    for i in range(0, 20000, 4):
-        nibble = bits[i//8] >> (i%8) & 0xF
-        frequencies[nibble] += 1
-    chi_squared = 16/5000 * sum(f**2 for f in frequencies) - 5000
-    return 1.03 < chi_squared < 57.4
+    bit_string = ''.join(f'{byte:08b}' for byte in bits)
 
+    # Split the binary string into segments of length m (in this case, 4)
+    m = 4
+    k = len(bit_string) // m
+    bit_segments = [bit_string[i * m : (i + 1) * m] for i in range(k)]
+
+    # Count the number of occurrences of each possible segment
+    counts = {f'{i:04b}': 0 for i in range(2**m)}
+    for segment in bit_segments:
+        counts[segment] += 1
+
+    x3 = (16 / k) * sum(count**2 for count in counts.values()) - k
+    print(f'Poker test: X3 is {x3}, acceptable range is 1.03 to 57.4, test {"passed" if 1.03 <= x3 <= 57.4 else "failed"}')
+
+    return 1.03 <= x3 <= 57.4
+
+# Have problome with it I was not able to get truly rundomize, if I reduce number bits I will pass the test.
 def runs_test(bits):
-    bit_string = ''.join(format(byte, '08b') for byte in bits)
+    bit_string = ''.join(f'{byte:08b}' for byte in bits)
+    
+    # Count the number of runs of each length from 1 to 6 (counting runs of length greater than 6 as 6)
     run_lengths = [len(run) for run in re.findall(r'1+|0+', bit_string)]
-    acceptable_runs = [(2267, 2733), (1079, 1421), (502, 748), (223, 402), (90, 223), (90, 223)]
-    results = [lower <= run_lengths.count(n) <= upper for n, (lower, upper) in enumerate(acceptable_runs, 1)]
-    for n, (lower, upper), result in zip(range(1, 7), acceptable_runs, results):
-        print(f'Runs of length {n}: count is {run_lengths.count(n)}, acceptable range is {lower} to {upper}, test {"passed" if result else "failed"}')
-    return all(results)
+    counts = {i: run_lengths.count(i) for i in range(1, 7)}
+    counts[6] += sum(run_lengths.count(i) for i in range(7, len(run_lengths) + 1))
+
+    limits = {
+        1: (2267, 2733),
+        2: (1079, 1421),
+        3: (502, 748),
+        4: (223, 402),
+        5: (90, 223),
+        6: (90, 223),
+    }
+
+    # Check if the counts of each run length are within the acceptable intervals
+    passed = True
+    for run_length, (lower, upper) in limits.items():
+        count = counts.get(run_length, 0)
+        if not lower <= count <= upper:
+            print(f'Runs of length {run_length}: count is {count}, acceptable range is {lower} to {upper}, test failed')
+            passed = False
+        else:
+            print(f'Runs of length {run_length}: count is {count}, acceptable range is {lower} to {upper}, test passed')
+    return passed
+
 
 def fips_140(bits):
     test_results = {
@@ -41,12 +86,3 @@ def fips_140(bits):
 
 bits = generate_random_sequence()
 test_results = fips_140(bits)
-
-for test_name, result in test_results.items():
-    print(f'{test_name}: {"Passed" if result else "Failed"}')
-
-if all(test_results.values()):
-    print("All tests passed, the 20000 bits sequence is random.")
-else:
-    print("The 20000 bits sequence is not random.")
-
